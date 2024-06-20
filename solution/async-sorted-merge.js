164ac07f1fc2ed7b533c641bbb4579e599617146
davidenwang <davidw1339@gmail.com>
2024-06-20 01:38:32 -0700

    min-heap based solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,35 @@
 "use strict";
 
+const { Heap } = require("heap-js");
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+  // Create min-heap with custom log comparator
+  const logComparator = ({ logEntry: a }, { logEntry: b }) =>
+    a.date.getTime() - b.date.getTime();
+  const logQueue = new Heap(logComparator);
+
+  // Populate min-heap with data from all log sources
+  for (const logSource of logSources) {
+    const logEntry = await logSource.popAsync();
+
+    if (!!logEntry) {
+      logQueue.push({ logEntry, logSource });
+    }
+  }
+
+  // Continuously pop/push off min-heap until all sources are drained
+  while (logQueue.length) {
+    const { logEntry, logSource } = logQueue.pop();
+    printer.print(logEntry);
+
+    const newEntry = await logSource.pop();
+    if (!!newEntry) {
+      logQueue.push({ logEntry: newEntry, logSource });
+    }
+  }
+  printer.done();
+
+  return console.log("Async sort complete.");
 };
